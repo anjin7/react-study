4987b1c7ea54e1142ee026ed58d0fe99f0f6d7e1
_An_Jin_ <aji0719@gmail.com>
2023-09-08 15:22:49 +0900

    day5-1

--- movie-web/src/App.js
+++ movie-web/src/App.js
@@ -1,9 +1,11 @@
-
+import Button from './Button';
+import styled from './App.module.css'
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <h1 className={styled.title}>Hello</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
