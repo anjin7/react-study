a75e64f621a59ca86c74293bf4eb062750c5627e
_An_Jin_ <aji0719@gmail.com>
2023-09-10 01:59:37 +0900

    challenge

--- movie-web/src/App.js
+++ movie-web/src/App.js
@@ -11,7 +11,7 @@ function App() {
           <Route path="/hello">
             <h2>Hello</h2>
           </Route>
-          <Route path="/movie/:id">
+          <Route path="/character/:id">
             <Detail />
           </Route>
           <Route path="/">
