c4fe4030dd6c038487201b1081ba4b05f8a6e557
_An_Jin_ <aji0719@gmail.com>
2023-09-10 00:57:59 +0900

    day7

--- movie-web/src/App.js
+++ movie-web/src/App.js
@@ -1,40 +1,27 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './App.module.css';
+import Home from './routes/Home';
+import Detail from './routes/Detail'
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [...currentArray, toDo]);
-    setToDo("");
-    
-  };
-  // console.log(toDos);
-  // console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
-  return (
-    <div className="App">
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+  return(
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/hello">
+            <h2>Hello</h2>
+          </Route>
+          <Route path="/movie/:id">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+          
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
