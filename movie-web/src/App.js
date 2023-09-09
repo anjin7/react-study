f20ab0725a0fa488f702ad60d65f9bd0f4c5e92a
_An_Jin_ <aji0719@gmail.com>
2023-09-09 17:28:47 +0900

    day5-3

--- movie-web/src/App.js
+++ movie-web/src/App.js
@@ -3,13 +3,11 @@ import styled from './App.module.css';
 import { useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
-  console.log("call an api");
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div className="App">
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <button onClick={onClick}>{ showing ? "Hide" : "Show" }</button>
     </div>
   );
 }
