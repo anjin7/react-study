f1d2135c5f98e17c3580567ba79873bd4ebcb9a8
_An_Jin_ <aji0719@gmail.com>
2023-09-10 13:31:25 +0900

    challenge

--- movie-web/src/routes/Home.js
+++ movie-web/src/routes/Home.js
@@ -1,8 +1,40 @@
-function Home() {
+import { useEffect, useState} from 'react';
+import Character from '../components/Character';
+import styles from "./Home.module.css";
+
+function Home(){
+  const [loading, setLoading] = useState(true);
+  const [characters, setCharacters] = useState([]);
+  const getCharacters = async() => {
+    const json = await (
+      await fetch(
+        `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023`
+      )
+    ).json();
+    setCharacters(json.data.results);
+    setLoading(false)
+  };
+  useEffect(() => {
+    getCharacters();
+  }, []);
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      {loading ? (
+      <h2 className={styles.loader}>Loading...</h2>
+      ) : (
+      <div className={styles.characters}>
+        {characters.map(character => (
+          <Character
+            key={character.id}
+            id={character.id}
+            coverImg={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+            heroName={character.title_long} 
+            description={character.description}
+          />
+        ))}
+      </div>
+      )}
     </div>
-  )
-}
+  );
+};
 export default Home;
