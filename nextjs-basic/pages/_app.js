b5203530d17c2f7047f238ee683b52af78ef3273
_An_Jin_ <aji0719@gmail.com>
2023-09-12 21:42:15 +0900

    nextJS 2.1

--- nextjs-basic/pages/_app.js
+++ nextjs-basic/pages/_app.js
@@ -1,11 +1,9 @@
 import Layout from '../components/Layout';
-import NavBar from "../components/NavBar";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <NavBar />
       <Component {...pageProps} />
     </Layout>
   );
