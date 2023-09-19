9458bfacfe28bc0f0d942e56bb20fc5c8082f088
_An_Jin_ <aji0719@gmail.com>
2023-09-19 19:29:52 +0900

    day 15-5

--- reactjs-masterclass/src/App.js
+++ reactjs-masterclass/src/App.js
@@ -2,6 +2,10 @@ import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
 `;
 const rotationAnimation = keyframes`
   0% {
@@ -16,6 +20,9 @@ const rotationAnimation = keyframes`
     border-radius:0px;
   }
 `;
+const Emoji = styled.span`
+  font-size: 36px;
+`;
 const Box = styled.div`
   height: 200px;
   width: 200px;
@@ -24,23 +31,16 @@ const Box = styled.div`
   justify-content: center;
   align-items: center;
   animation: ${rotationAnimation} 3s linear infinite;
-  span {
-    font-size: 36px;
-    &:hover {
-      font-size: 48px;
-    }
-    &:active {
-      opacity: 0;
-    }
+  ${Emoji}:hover {
+    font-size: 98px;
   }
 `;
-
 
 function App() {
   return (
     <Wrapper>
       <Box>
-        <span>🤩</span>
+        <Emoji>🤩</Emoji>
       </Box>
     </Wrapper>
   );
