f887187329a6bbcaa6a0cddeb4c438ac58506f4b
_An_Jin_ <aji0719@gmail.com>
2023-09-20 18:27:17 +0900

    day 17-2

--- reactjs-ts/src/index.tsx
+++ reactjs-ts/src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ThemeProvider } from "styled-components";
-import { darkTheme, lightTheme } from "./theme";
+
+import { RouterProvider } from 'react-router-dom';
+import router from './Router';
+
+
 
 
 const root = ReactDOM.createRoot(
@@ -10,9 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
