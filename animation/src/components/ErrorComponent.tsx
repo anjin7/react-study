658e641febbc58378b93d92b55e19992953a61e6
_An_Jin_ <aji0719@gmail.com>
2023-09-29 00:42:01 +0900

    challenge 17

--- animation/src/components/ErrorComponent.tsx
+++ animation/src/components/ErrorComponent.tsx
@@ -1,6 +1,23 @@
+import { styled } from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  margin: 0 auto;
+  height: 100vh;
+  width: 100vw;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h2`
+  font-size: 36px;
+  font-weight: 700;
+`;
 function ErrorComponent() {
   return (
-    <div>This Component crushed</div>
+    <Container>
+      <Title>This Component Crushed</Title>
+    </Container>
   )
 }
 
--- animation/src/screens/NotFound.tsx
+++ animation/src/screens/NotFound.tsx
@@ -1,6 +1,24 @@
+import { styled } from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  margin: 0 auto;
+  height: 100vh;
+  width: 100vw;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h2`
+  font-size: 36px;
+  font-weight: 700;
+`;
+
 function NotFound() {
   return (
-    <div>404 NOT FOUND</div>
+    <Container>
+      <Title>404 NOT FOUND</Title>
+    </Container>
   )
 }
 
