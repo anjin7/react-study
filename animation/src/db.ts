66e4e9b50604c5d82eb406be3f0a3a50cbef03f7
_An_Jin_ <aji0719@gmail.com>
2023-09-28 20:39:48 +0900

    challenge 17

---
+++ animation/src/db.ts
@@ -0,0 +1,56 @@
+export const authors = [
+  {
+    id: 1,
+    name: "Tony_Stark",
+    book: [
+      {
+        id: 11,
+        title: "IronMan_1",
+        chapter: ["chapter1", "chapter2", "chapter3"],
+        character: ["Tony Stark", "Pepper Potts", "Obadiah Stane"]
+      },
+      {
+        id: 12,
+        title: "IronMan_2",
+        chapter: ["chapter1", "chapter2"],
+        character: ["Tony Stark", "Pepper Potts", "Happy Hogan"]
+      },
+      {
+        id: 13,
+        title: "IronMan_3",
+        chapter: ["chapter1", "chapter2", "chapter3", "chapter4"],
+        character: ["Tony Stark", "Pepper Potts", "Happy Hogan"]
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: "Steve_Rogers",
+    book: [
+      {
+        id: 21,
+        title: "Captain_America:_The_First_Avenger",
+        chapter: ["chapter1", "chapter2", "chapter3"],
+        character: ["Steve Rogers", "James Barnes", "	Peggy Carter"]
+      },
+      {
+        id: 22,
+        title: "Captain_America:_The_Winter_Soldier",
+        chapter: ["chapter1", "chapter2", "chapter3", "chapter4"],
+        character: ["Steve Rogers", "James Barnes", "Sam Wilson"]
+      },
+      {
+        id: 23,
+        title: "Captain_America:_Civil_War",
+        chapter: ["chapter1", "chapter2", "chapter3", "chapter4", "chapter5"],
+        character: [
+          "Steve Rogers",
+          "James Barnes",
+          "Sam Wilson",
+          "Natasha Romanoff",
+          "Wanda Maximoff "
+        ]
+      }
+    ]
+  }
+];
---
+++ animation/src/screens/About.tsx
@@ -0,0 +1,12 @@
+
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>About Marvel Movie Character</p>
+    </div>
+    
+  )
+}
+export default About;
