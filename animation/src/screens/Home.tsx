76927265e8edeb8b8ca3ae48cc55bd5298ad9012
_An_Jin_ <aji0719@gmail.com>
2023-09-30 20:00:17 +0900

    challenge 17

--- animation/src/screens/Home.tsx
+++ animation/src/screens/Home.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from 'react';
 
 
 const Wrapper = styled.div`
@@ -17,6 +18,16 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const boxVariants = {
   hover: { scale: 1.5, rotateZ: 90 },
   click: { scale: 1, borderRadius: "100px" },
@@ -24,6 +35,7 @@ const boxVariants = {
 };
 
 function Home() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
       <Box
@@ -33,6 +45,17 @@ function Home() {
         whileDrag="drag"
         whileTap="click"
       />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.6}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileTap="click"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 };
