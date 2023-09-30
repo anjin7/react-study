bb17f79c291eba316a92deeef033a074016fa3b7
_An_Jin_ <aji0719@gmail.com>
2023-09-30 15:31:17 +0900

    challege 17

--- animation/src/screens/Home.tsx
+++ animation/src/screens/Home.tsx
@@ -9,20 +9,30 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const boxVariants = {
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "rgb(24, 25, 102)", transition: { duration: 10 } },
+};
+
 function Home() {
   return (
     <Wrapper>
-      <Box />
-      <div></div>
-      <motion.div></motion.div>
+      <Box
+        drag
+        variants={boxVariants}
+        whileHover="hover"
+        whileDrag="drag"
+        whileTap="click"
+      />
     </Wrapper>
   );
 };
