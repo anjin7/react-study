40e942adc9eb238088b613094a2e201b205df81a
_An_Jin_ <aji0719@gmail.com>
2023-10-28 02:26:39 +0900

    challenge 29

--- carrot/pages/challenge/index.tsx
+++ carrot/pages/challenge/index.tsx
@@ -8,33 +8,64 @@ const Home: NextPage = () => {
         <h1 className="text-center mt-8 mb-16 text-3xl font-extrabold">
           Job Application Form
         </h1>
+
         <form>
-          <div className="mb-6">
-            <h2 className="text-lg font-extrabold">
+          <div className="mb-8">
+            <h2 className="text-xl font-extrabold">
               What department do you want to work for?
             </h2>
+            <div className="my-2">
+              <input type="radio" name="department" value="sales" />
+              <label className="font-bold mx-2" htmlFor="sales">
+                Sales
+              </label>
+            </div>
+            <div className="my-2">
+              <input type="radio" name="department" value="marketing" />
+              <label className="font-bold mx-2" htmlFor="marketing">
+                Marketing
+              </label>
+            </div>
+            <div className="my-2">
+              <input type="radio" name="department" value="accounting" />
+              <label className="font-bold mx-2" htmlFor="accounting">
+                Accounting
+              </label>
+            </div>
+            <div className="my-2">
+              <input type="radio" name="department" value="service" />
+              <label className="font-bold mx-2" htmlFor="service">
+                Cunstomer Service
+              </label>
+            </div>
           </div>
-          <div className="mb-6">
-            <h2 className="text-lg font-extrabold">
+
+          <div className="mb-8">
+            <h2 className="text-xl font-extrabold">
               Why do you want to join this company?
             </h2>
           </div>
           <div className="mb-6">
-            <h2 className="text-lg font-extrabold">Salary</h2>
+            <h2 className="text-xl font-extrabold">Salary</h2>
           </div>
           <div className="mb-6">
-            <h2 className="text-lg font-extrabold">Introduce your self</h2>
+            <h2 className="text-xl font-extrabold">Introduce your self</h2>
           </div>
         </form>
         <div className="mb-6">
-          <h2 className="text-lg font-extrabold">
+          <h2 className="text-xl font-extrabold">
             Tell us what your dreams are
           </h2>
         </div>
         <div className="mb-6">
-          <h2 className="text-lg font-extrabold">Email</h2>
+          <h2 className="text-xl font-extrabold">Email</h2>
         </div>
-        <button className="rounded-2xl text-lg font-extrabold w-full px-24 py-6 shadow-xl bg-yellow-300 hover:bg-yellow-100 border-2 border-b-[6px] border-r-[6px] border-black">
+        <button
+          className="rounded-2xl text-lg font-extrabold w-full 
+        px-24 py-6 shadow-xl bg-yellow-300 hover:bg-yellow-100 border-2 
+        border-b-[6px] border-r-[6px] border-black focus:border-[2px] 
+        focus:border-t-[6px] focus:border-l-[6px] focus:bg-sky-200"
+        >
           Give me this job
         </button>
       </div>
