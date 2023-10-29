1301fd726adbd765bd1069e285d61685efe9314e
_An_Jin_ <aji0719@gmail.com>
2023-10-29 20:00:56 +0900

    challenge

--- carrot/pages/challenge/index.tsx
+++ carrot/pages/challenge/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import type { NextPage } from "next";
+import { useForm } from "react-hook-form";
 
 const Home: NextPage = () => {
   return (
@@ -15,25 +16,40 @@ const Home: NextPage = () => {
               What department do you want to work for?
             </h2>
             <div className="my-2">
-              <input type="radio" name="department" value="sales" />
+              <input type="radio" name="department" value="sales" id="sales" />
               <label className="font-bold mx-2" htmlFor="sales">
                 Sales
               </label>
             </div>
             <div className="my-2">
-              <input type="radio" name="department" value="marketing" />
+              <input
+                type="radio"
+                name="department"
+                value="marketing"
+                id="marketing"
+              />
               <label className="font-bold mx-2" htmlFor="marketing">
                 Marketing
               </label>
             </div>
             <div className="my-2">
-              <input type="radio" name="department" value="accounting" />
+              <input
+                type="radio"
+                name="department"
+                value="accounting"
+                id="accounting"
+              />
               <label className="font-bold mx-2" htmlFor="accounting">
                 Accounting
               </label>
             </div>
             <div className="my-2">
-              <input type="radio" name="department" value="service" />
+              <input
+                type="radio"
+                name="department"
+                value="service"
+                id="service"
+              />
               <label className="font-bold mx-2" htmlFor="service">
                 Cunstomer Service
               </label>
@@ -44,30 +60,54 @@ const Home: NextPage = () => {
             <h2 className="text-xl font-extrabold">
               Why do you want to join this company?
             </h2>
+            <div className="my-2">
+              <input type="radio" name="want" value="money" id="money" />
+              <label className="font-bold mx-2" htmlFor="money">
+                I want money!
+              </label>
+            </div>
+            <div className="my-2">
+              <input type="radio" name="want" value="love" id="love" />
+              <label className="font-bold mx-2" htmlFor="love">
+                I love this company
+              </label>
+            </div>
+            <div className="my-2">
+              <input type="radio" name="want" value="learn" id="learn" />
+              <label className="font-bold mx-2" htmlFor="learn">
+                I want to learn
+              </label>
+            </div>
+            <div className="my-2">
+              <input type="radio" name="want" value="why" id="why" />
+              <label className="font-bold mx-2" htmlFor="why">
+                I dont know why
+              </label>
+            </div>
           </div>
-          <div className="mb-6">
+          <div className="mb-8">
             <h2 className="text-xl font-extrabold">Salary</h2>
           </div>
-          <div className="mb-6">
+          <div className="mb-8">
             <h2 className="text-xl font-extrabold">Introduce your self</h2>
           </div>
         </form>
-        <div className="mb-6">
+        <div className="mb-8">
           <h2 className="text-xl font-extrabold">
             Tell us what your dreams are
           </h2>
         </div>
-        <div className="mb-6">
+        <div className="mb-8">
           <h2 className="text-xl font-extrabold">Email</h2>
         </div>
-        <button
+        <input
+          type="submit"
+          value="Give me this job"
           className="rounded-2xl text-lg font-extrabold w-full 
         px-24 py-6 shadow-xl bg-yellow-300 hover:bg-yellow-100 border-2 
         border-b-[6px] border-r-[6px] border-black focus:border-[2px] 
         focus:border-t-[6px] focus:border-l-[6px] focus:bg-sky-200"
-        >
-          Give me this job
-        </button>
+        />
       </div>
     </div>
   );
