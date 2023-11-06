9ba5aebaf95f30de83662cac0d64e4dabc336e04
_An_Jin_ <aji0719@gmail.com>
2023-11-06 17:08:04 +0900

    carrot

--- carrot/pages/api/users/enter.tsx
+++ carrot/pages/api/users/enter.tsx
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
+import twilio from 'twilio';
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
