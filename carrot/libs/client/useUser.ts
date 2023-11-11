b0dac0266524af780ad86cc4554505f7299d4251
_An_Jin_ <aji0719@gmail.com>
2023-11-11 20:07:14 +0900

    carrot 10.3

--- carrot/libs/client/useUser.ts
+++ carrot/libs/client/useUser.ts
@@ -1,22 +1,21 @@
-import { User } from "@prisma/client";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-interface ProfileResponse {
-  ok: boolean;
-  profile: User;
-}
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export default function useUser() {
-  const { data, error } = useSWR<ProfileResponse>(
-    typeof window === "undefined" ? null : "/api/users/me"
-  );
+  const [user, setUser] = useState();
   const router = useRouter();
   useEffect(() => {
-    if (data && !data.ok) {
-      router.replace("/enter");
-    }
-  }, [data, router]);
-  return { user: data?.profile, isLoading: !data && !error };
+    fetch("/api/users/me")
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data.ok) {
+          return router.replace("/enter");
+        }
+        setUser(data.profile);
+      });
+  }, [router]);
+  return user;
 }
